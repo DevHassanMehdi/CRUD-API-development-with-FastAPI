dd30c0340edc9a3be0c45debbd84f99e4173643a
hassanmehdi <hassanmehdi510@gmail.com>
2023-03-27 02:25:43 +0500

    Added post submit and retrieve

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI  # The fastAPI
 from fastapi.params import Body  # For receiving data in post requests
 from pydantic import BaseModel  # To specify a schema of what the post request data should look like
 from typing import Optional  # In case we want to add some optional data a user may or may not send with POST request
+from random import randrange  # To temporarily create random Ids when working without database
 
 app = FastAPI()
 
@@ -13,24 +14,34 @@ class Post(BaseModel):
 	content: str
 	# Optional. If not provided by user, set a default value or None
 	published: bool = True
-	rating: Optional[int] = None
+	rating: Optional[float] = None
+
+
+my_posts = [
+	{'title': 'this is the first post',
+		'content': 'this is the content',
+		'published': False,
+		'rating': 2.4,
+		"id": 54351},
+	{'title': 'this is the second post',
+		'content': 'this is the content',
+		'published': False,
+		'rating': 4.0,
+		"id": 13876}]
 
 
 # Path Operation
 # Get request
-@app.get("/")
+@app.get("/posts")
 def root():
-	return {"message": "Hello"}
+	return {"data": my_posts}
 
 
 # Path Operation
 # POST request
 @app.post("/posts")
 def create_post(post: Post):  # Receive data from post and validate it with the Post class schema
-	print(post.title)
-	print(post.content)
-	print(post.published)
-	print(post.rating)
-	print(post.dict())
-	
-	return {"Data": f"{post}"}
+	post_dict = post.dict()
+	post_dict["id"] = randrange(1, 9999999)
+	my_posts.append(post_dict)
+	return {"Data": post_dict}
