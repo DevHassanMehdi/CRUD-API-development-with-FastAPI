6671fe1e8440bdf94692560625c6db79901ebe9e
hassanmehdi <hassanmehdi510@gmail.com>
2023-04-02 20:36:20 +0500

    Refactored the databse initialization code

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQL Alchemy connection URL
-SQLALCHEMY_DATABASE_URL = 'postgres://postgres:root@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/fastapi'
 
 # SQL Alchemy connection engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -13,3 +13,12 @@ session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class used to create models
 Base = declarative_base()
+
+
+# Create session to get the database
+def get_db():
+	db = session_local()
+	try:
+		yield db
+	finally:
+		db.close()
