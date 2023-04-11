50942c1db0a958bfba0f9aaa4850d9b0694f4b80
hassanmehdi <hassanmehdi510@gmail.com>
2023-04-11 21:02:22 +0500

    Changed default to server_default in table for published column

--- app/models.py
+++ app/models.py
@@ -9,4 +9,4 @@ class Post(Base):
 	id = Column(Integer, primary_key=True, nullable=False)
 	title = Column(String, nullable=False)
 	content = Column(String, nullable=False)
-	published = Column(Boolean, nullable=False, default=False)
+	published = Column(Boolean, server_default="True", nullable=False)
