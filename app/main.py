251aeacce734ac32999fc8e2e8f448d7fcf86df9
hassanmehdi <hassanmehdi510@gmail.com>
2023-04-11 23:11:49 +0500

    Updated the queries in all path operation with the SQLAlchemy ORM

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 import time  # To add time pause between connection failing and retry connecting to the database
 
-import psycopg2  # Library to deal with our Postgres database
 from fastapi import FastAPI  # The fastAPI
-from fastapi import HTTPException, status  # To raise appropriate HTTP Exception and set http status
+from fastapi import HTTPException, status, Depends, Response  # To raise appropriate HTTP Exception and set http status
+
 from psycopg2.extras import RealDictCursor  # To get the column names when we return something from the database
 from pydantic import BaseModel  # To specify a schema of what the post request data should look like
+import psycopg2  # Library to deal with our Postgres database
 
+from sqlalchemy.orm import Session
+from sqlalchemy import desc
+
+from .database import engine, get_db  # Session from database.py file
 from . import models  # Models.py file with all the defined tables
-from .database import engine  # Session from database.py file
 
 # To create all the defined models. These models are the tables we defined in the models.py file.
 models.Base.metadata.create_all(bind=engine)
@@ -39,95 +43,69 @@ while True:
 		print("Error:", error)
 		time.sleep(2)
 
-my_posts = [
-	{
-		'title': 'this is the first post',
-		'content': 'this is the content',
-		'published': False,
-		'rating': 2.4,
-		"id": 54351},
-	{
-		'title': 'this is the second post',
-		'content': 'this is the content',
-		'published': False,
-		'rating': 4.0,
-		"id": 13876}]
-
-
-# A method to find the specific post requested by user in get request by id
-def find_post(post_id):
-	for post in my_posts:
-		if post["id"] == post_id:
-			return post
-
-
-# A method to find the specific post and return its index
-def find_post_index(post_id):
-	for index, post in enumerate(my_posts):
-		if post['id'] == post_id:
-			return index
-
 
 # Path Operations
 
 # Create a post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):  # Receive data from post and validate it with the Post class schema
-	cursor.execute(
-		"""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-		(post.title, post.content, post.published))
-	new_post = cursor.fetchone()
-	connection.commit()
-	return {"Post Created": new_post}
+def create_post(post: Post, db: Session = Depends(get_db)):  # Get post data from user
+	new_post = models.Post(**post.dict())  # Query to add new post to db
+	db.add(new_post)  # Run query
+	db.commit()  # Commit changes
+	db.refresh(new_post)  # Update the var with new post
+	return {"Post Created": new_post}  # Return post to the user
 
 
 # Get all posts
 @app.get("/posts", status_code=status.HTTP_200_OK)
-def get_all_posts():
-	cursor.execute("""SELECT * FROM posts""")
-	posts = cursor.fetchall()
-	return {"All Posts": posts}
+def get_all_posts(db: Session = Depends(get_db)):
+	posts = db.query(models.Post).all()  # Query to get all the posts
+	return {"All Posts": posts}  # Return all posts to the user
 
 
 # Find the latest post
 @app.get("/posts/latest", status_code=status.HTTP_200_OK)
-def get_latest_post():
-	cursor.execute("""SELECT * FROM posts ORDER BY id DESC LIMIT 1""")
-	post = cursor.fetchone()
-	if not post:  # If item not found set http status to 404
+def get_latest_post(db: Session = Depends(get_db)):
+	post = db.query(models.Post).order_by(desc(models.Post.created_at)).first()  # Query to get the latest post
+	
+	if not post:  # If post not found set http status to 404
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No posts yet.")
-	return {"Latest Post": post}
+	return {"Latest Post": post}  # Return the latest post
 
 
 # Find a specific post using the post id
 @app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
-def get_post(post_id: int):  # The post id should be int.
-	cursor.execute("""SELECT * FROM posts WHERE id = %s""", str(post_id))
-	post = cursor.fetchone()
-	if not post:  # If item not found set http status to 404
+def get_post(post_id: int, db: Session = Depends(get_db)):  # The post id should be int.
+	post = db.query(models.Post).filter(models.Post.id == post_id).first()  # Query to get the specific post
+	if not post:  # If post not found set http status to 404
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found.")
-	return {"Post found": post}
+	return {"Post found": post}  # Return post
 
 
 # Update Post
 @app.put("/posts/{post_id}")
-def update_post(post_id: int, post: Post):
-	cursor.execute(
-		"""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-		(post.title, post.content, post.published, str(post_id)))
-	updated_post = cursor.fetchone()
-	connection.commit()
-	if updated_post is None:
+def update_post(post_id: int, post: Post, db: Session = Depends(get_db)):
+	post_query = db.query(models.Post).filter(models.Post.id == post_id)  # Query to get the specific post
+	old_post = post_query.first()  # The post itself
+	
+	if old_post is None:  # If post not found set http status to 404
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
-	return {"Post Updated": updated_post}
+	
+	post_query.update(post.dict(), synchronize_session=False)  # update the post with user provided info
+	db.commit()  # Commit changes
+	db.refresh(old_post)  # Refresh the old post
+	updated_post = old_post
+	return {"Post Updated": updated_post}  # Return the updated post
 
 
 # Delete a post
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int):
-	cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", str(post_id))
-	post = cursor.fetchone()
-	connection.commit()
-	if post is None:
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+	post = db.query(models.Post).filter(models.Post.id == post_id)  # Query to get the specific post
+	
+	if post.first() is None:  # If post not found set http status to 404
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
-	return {"Post Deleted": post}
+	
+	post.delete(synchronize_session=False)  # Delete the post
+	db.commit()  # Commit changes to the database
+	return Response(status_code=status.HTTP_204_NO_CONTENT)  # Return positive response
